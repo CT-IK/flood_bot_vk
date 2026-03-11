1bf2939d181e0bd9b077e9b0543d9121bd38630c
Lyubo Yuriy <uralubo@gmail.com>
2026-03-11 21:43:09 +0300

    Yura:VK correctives

--- credentials.py
+++ credentials.py
@@ -1,8 +1,5 @@
 from typing import Final
 
-#Ключи доступа (токен преда для доступа к VK API, токен доступа группы (бота), секретный ключ сообщества и код доступа)
-VK_TOKEN: Final="vk1.a.aFDZSGbEEoh_N-upcLEstajh1-yFT02rDX8axeYkkZPWo8kotPBKYhZuFq2Vy7SmHifsMwHH9Rh1TSiamDcuJxMoHa8ip_CgS0Rkoy7Th6wByqt8l6YpufJdemx7uv0cpCSKa45fg4VCkqfXn4AhIIdA6F5AlgBiVJLbXjHP4U-wWp-3wvFtslFl-R92u-SYXJVlgtuEdfHHhiABR-9rWA"#Важный параметр
-
 #ВК айди разработчиков
 DEVS: Final=000000000
 
